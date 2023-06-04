586ff41478a96cf76643e84f4be8fc2d54a7c834
1nian <531663830@qq.com>
2023-06-04 20:12:26 +0800

    feat: 添加入口文件

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require('path');
+const history = require('connect-history-api-fallback')
+
+app.use(history())
+app.use(express.static(path.join(__dirname,'dist'))) 
+
 const server = http.createServer(app);
+
 const { Server } = require("socket.io");
 const io = new Server(server,{
     cors: true
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
 io.on("connection", (socket) => {
   socket.on('update:message', (msg) => {
@@ -19,6 +26,6 @@ io.on("connection", (socket) => {
   socket.broadcast.emit('hello')
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(3101, () => {
+  console.log("listening on *:3101");
 });
